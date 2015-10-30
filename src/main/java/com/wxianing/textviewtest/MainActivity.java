3b988da47166f8f7e7a2a2525d49a694bf39832f
wxianing <wxianing@163.com>
2015-10-30 22:27:07 +0800

    Initial commit

---
+++ src/main/java/com/wxianing/textviewtest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wxianing.textviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
